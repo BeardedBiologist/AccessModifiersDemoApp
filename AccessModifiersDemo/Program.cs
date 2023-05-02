01d7ddf3bc5eecf25e605f32170c7df663cdefa9
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-02 10:51:12 +0200

    init commit

--- AccessModifiersDemo/Program.cs
+++ AccessModifiersDemo/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
 
+// Mini project to use inheritance and interfaces
+
+namespace AccessModifiersDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            Console.ReadLine();
+        }
+    }
+
+}
